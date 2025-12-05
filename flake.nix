561110a5a35610c661ad6aa9225f369023bb08e4
pebble <166765742+ignorant05@users.noreply.github.com>
2025-12-05 16:28:16 +0100

    Feature: added flake.nix file

--- flake.nix
+++ flake.nix
@@ -9,54 +9,75 @@
     };
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    erlang.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = 
-    { self, 
-      nixpkgs,
-      flake-parts,
-      treefmt-nix,
-      ... 
-    }@inputs:
+  outputs = { self, nixpkgs, flake-parts, treefmt-nix, erlang, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem =
-        { pkgs, system, ... }:
-          let
-            treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-          in
-          {
-            # This sets `pkgs` to a nixpkgs with allowUnfree option set.
-            _module.args.pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
+      perSystem = { pkgs, system, ... }: 
+        let
+          # Fetching rebar3_nix plugin  
+          rebar3_nix = pkgs.fetchFromGitHub {
+            owner = "erlang-nix";
+            repo = "rebar3_nix";
+            rev = "v0.1.1"; 
+            sha256 = "sha256-Powzl56wtB0yvMMBvxdaDeqb9qqHD4/BxrDsjQ1gMoI=";
+          };
 
-            # nix build
-            packages = {
-              hello = pkgs.hello;
-              default = pkgs.hello;
-            };
+          erlangPlatform = pkgs.beam.interpreters.erlang_28;
 
-            devShells = 
-              {
-                default = pkgs.mkShell {
-                  packages = with pkgs; [
-                    erlang_28
-                    erlang-language-platform
-                    rebar3
-                  ];
-                };
-              };
+          buildRebar3 = pkgs.beamPackages.buildRebar3 {
+            pname = "domino";     
+            version = "0.1.0";
+            src = ./.;           
+            beam = erlangPlatform;
+            generate-lock = true; 
 
-            # nix fmt
-            formatter = treefmtEval.config.build.wrapper;
+            name = "domino";     
 
-            # nix flake check --all-systems
-            checks = {
-              formatting = treefmtEval.config.build.check self;
+            buildInputs = [ rebar3_nix ];
+
+            meta = with pkgs.lib; {
+              description = "Domino Erlang Server";
+              license = pkgs.lib.licenses.mit;
+              platforms = pkgs.lib.platforms.all;
             };
           };
+
+        in {
+          # This sets `pkgs` to a nixpkgs with allowUnfree option set.
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+
+          # nix build
+          packages = {
+            domino = buildRebar3;  
+            default = buildRebar3; 
+          };
+
+          # Define devShell for Erlang + Rebar3
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                erlang_28
+                erlang-language-platform
+                rebar3
+              ];
+            };
+          };
+
+          # Formatter setup for Nix files
+          formatter = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+          # nix flake check --all-systems
+          checks = {
+            formatting = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+          };
+        };
     };
 }
