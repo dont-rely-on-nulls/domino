f9097a0562ea8b40d40a19059578a9280b945de4
Marcos Magueta <maguetamarcos@gmail.com>
2026-05-18 12:41:08 -0300

    cleanup root

--- shared/librelational_engine/library.h
+++
@@ -1,8 +0,0 @@
-#ifndef RELATIONAL_ENGINE_LIBRARY_H
-#define RELATIONAL_ENGINE_LIBRARY_H
-
-#define HASH_SIZE 64
-
-extern void compute_hash(char*, const char*);
-
-#endif //RELATIONAL_ENGINE_LIBRARY_H
