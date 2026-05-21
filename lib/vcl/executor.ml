65c533ef102ea71b8420d556ae3a9567f17effd8
Estevan Castilho <tevo@riajuu.net>
2026-05-21 01:49:20 +0000

    Migrate the VCL over

--- lib/vcl/executor.ml
+++ lib/vcl/executor.ml
@@ -1,21 +1,11 @@
 module Make (NT : Nt.S) = struct
-  type error =
-    | ParseError of string
-    | NtError of Nt.error
-
-  let sexp_of_error e =
-    let open Sexplib.Sexp in
-    match e with
-    | ParseError s -> List [ Atom "parse-error"; Atom s ]
-    | NtError e    -> List [ Atom "nt-error"; Atom (Nt.string_of_error e) ]
-
   let execute (ctx : Sublanguage_context.t) (stmt : Ast.statement) :
-      (Sublanguage_types.result, error) result =
+      (Sublanguage_types.result, Condition.t) result =
     match stmt with
     | Ast.Use branch_name ->
-        match ctx.switch_branch branch_name with
-        | Ok delta     -> Ok (Sublanguage_types.Transition delta)
-        | Error e      -> Error (NtError e)
+       Result.map
+         (fun delta -> Sublanguage_types.Transition delta)
+         (ctx.switch_branch branch_name)
 
   let _ = (module NT : Nt.S)
 end
