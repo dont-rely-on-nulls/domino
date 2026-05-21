a3aeca4695f475464b7fecd88417bf14c56eeb1d
Estevan Castilho <tevo@riajuu.net>
2026-05-21 02:16:53 +0000

    Migrate the parsers over

--- lib/drl/parser.ml
+++ lib/drl/parser.ml
@@ -1,13 +1,16 @@
-type error = ParseError of string
+module Error = struct
+  open Condition
+  let parse_error msg = condition "parse-error" ("message" |=| (of_string msg))
+end
 
 let of_sexp sexp =
   match Ast.query_of_sexp sexp with
   | q -> Ok q
-  | exception exn -> Error (ParseError (Printexc.to_string exn))
+  | exception exn -> Error (Error.parse_error (Printexc.to_string exn))
 
 let of_string s =
   match Sexplib.Sexp.of_string s |> Ast.query_of_sexp with
   | q -> Ok q
-  | exception exn -> Error (ParseError (Printexc.to_string exn))
+  | exception exn -> Error (Error.parse_error (Printexc.to_string exn))
 
 let to_string q = Ast.sexp_of_query q |> Sexplib.Sexp.to_string_hum
--- lib/prl/parser.ml
+++ lib/prl/parser.ml
@@ -1,4 +1,7 @@
-type error = ParseError of string
+module Error = struct
+  open Condition
+  let parse_error msg = condition "parse-error" ("message" |=| (of_string msg))
+end
 
 let of_sexp sexp =
   let sexp =
@@ -6,7 +9,7 @@ let of_sexp sexp =
   in
   match Ast.statement_of_sexp sexp with
   | stmt -> Ok stmt
-  | exception exn -> Error (ParseError (Printexc.to_string exn))
+  | exception exn -> Error (Error.parse_error (Printexc.to_string exn))
 
 let of_string s = of_sexp (Sexplib.Sexp.of_string s)
 let to_string stmt = Ast.sexp_of_statement stmt |> Sexplib.Sexp.to_string_hum
